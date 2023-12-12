88967deabb269d0511f96406db418a2175e3be02
Lampiranta <topi.lampiranta@gmail.com>
2023-12-12 21:48:28 +0200

    Push sensor data to sql db

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import json
-import json
 import os
 import base64
 import pyodbc
+from sqlalchemy import create_engine, event
 import numpy as np
 import pandas as pd
 from pandas import DataFrame, json_normalize
+import dateutil.parser
 from datetime import datetime
 import uuid
+
 
 connect_str = os.environ['CONNSTR']
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
@@ -30,7 +32,7 @@ for blob in blob_list:
 
     payload = json.loads(body)
     body_df = json_normalize(payload)
-
+    print(body_df.dtypes)
     if len(body_df.columns) % 12 != 0:
         # Move blob to some error folder.
         print("Blob body is not in expected form.")
@@ -43,7 +45,7 @@ for blob in blob_list:
             ("bda", str),
             ("temperature", float),
             ("humidity", float),
-            ("pressure", float),
+            ("pressure", int),
             ("accelerometerX", float),
             ("accelerometerY", float),
             ("accelerometerZ", float),
@@ -57,11 +59,24 @@ for blob in blob_list:
     values_list = body_df.values[0]
     for i in range(int(len(body_df.columns) / 12)):
         meas_df.loc[i] = values_list[12 * i:12 * i + 12]
-    meas_df['time'] = blob_df.loc[:, "EnqueuedTimeUtc"].iloc[0]
-    print(meas_df)
+    d = dateutil.parser.parse(blob_df.loc[:, "EnqueuedTimeUtc"].iloc[0])
+    meas_df['date'] = d.strftime('%Y-%m-%d %H:%M:%S')
+    meas_df = meas_df.convert_dtypes()
+
+    DRIVER = 'ODBC Driver 18 for SQL Server'  # ODBC Driver
+    connection_string = 'mssql+pyodbc://{uid}:{password}@{server}:1433/{database}?driver={driver}'.format(
+        uid=os.environ['SERVERUSERNAME'],
+        password=os.environ['SERVERPASSWORD'],
+        server=os.environ['SERVERNAME'],
+        database=os.environ['DATABASENAME'],
+        driver=DRIVER.replace(' ', '+'))
+
+    table_name = 'home'
+    engine = create_engine(connection_string)
+    meas_df.to_sql(table_name, engine, index=False, if_exists='append', schema='weather')
 
     iterator += 1
 
     # This break is for debugging only, to only get first N blobs.
-    if iterator > 10:
+    if iterator > 0:
         break
